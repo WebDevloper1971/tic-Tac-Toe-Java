636960ee71746d34a62ef72739ea7cb1f8ebb9fb
Vedant <webtech1971@gmail.com>
2023-11-20 01:17:56 +0530

    Functioning Tic Tac Toe

--- Main.java
+++ Main.java
@@ -22,7 +22,7 @@ public class Main extends JFrame implements ActionListener{
     private JLabel title;
     private Random random = new Random();
 
-    private boolean playerX = false;
+    private boolean playerX;
 
     Main(){
 
@@ -42,34 +42,18 @@ public class Main extends JFrame implements ActionListener{
         buttonPanel.setBackground(Color.yellow);
         buttonPanel.setLayout(new GridLayout(3,3));
 
-        for (JButton btn : buttonsArray) {
-            btn = new JButton();
-            btn.addActionListener(this);
-            btn.setBackground(Color.black);
-            btn.setBorder(BorderFactory.createLineBorder(Color.white));
-            btn.setFocusable(false);
-            buttonPanel.add(btn);
+        for (int i = 0; i < 9; i++) {
+            buttonsArray[i] = new JButton();
+            buttonsArray[i].setBackground(Color.black);
+            buttonsArray[i].setBorder(BorderFactory.createLineBorder(Color.white));
+            buttonsArray[i].setFocusable(false);
+            buttonsArray[i].addActionListener(this);
+            buttonPanel.add(buttonsArray[i]);
         }
 
 
-        try {
-            
-            if (random.nextInt(2) == 1) {
-                playerX = true;
-                Thread.sleep(1000);
-
-                title.setText("Player X's Turn");
-                playerX = false;
-            }else{
-
-                Thread.sleep(1000);
-                title.setText("Player O's Turn");
-            }
-
-        }catch (Exception ex) {
-            // e.printStackTrace();
-            System.out.println("Sleeping Error");
-        }
+       
+       
 
 
         add(titlePanel,BorderLayout.NORTH);
@@ -81,22 +65,210 @@ public class Main extends JFrame implements ActionListener{
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+
+
+         firstTurn();
+
+        
     }
 
     
-
+    
        
 
-    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+       
+            for (int i = 0; i < buttonsArray.length; i++) {
+                if (e.getSource() == buttonsArray[i]) {
+                    if (playerX) {
+                        if (buttonsArray[i].getText() == "") {
+                            buttonsArray[i].setText("X");
+                            buttonsArray[i].setForeground(Color.red);
+                            buttonsArray[i].setFont(new Font ("Ink Free",Font.BOLD,30));
+                            playerX = false;
+                            title.setText("Players O's Turn");
+                            checkWinner();
+                        }
+
+                    }else{
+
+                         if (buttonsArray[i].getText() == "") {
+                            buttonsArray[i].setText("O");
+                            buttonsArray[i].setForeground(Color.blue);
+                            buttonsArray[i].setFont(new Font ("Ink Free",Font.BOLD,30));
+                            playerX = true;
+                            title.setText("Players X's Turn");
+                            checkWinner();
+                        }
+
+                    }
+                }
+            }
+        
+       
+        
+        
+       
+
+    }
+
+    public void firstTurn(){
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            System.out.println("Sleeping error");
+        }
+
+        if(random.nextInt(2) == 0){
+            playerX = true;
+            title.setText("Player X's Turn");
+        }else{
+            playerX = false;
+            title.setText("Player O's Turn");
+        }
+    }
 
     public void checkWinner(){
 
-        if (
-            buttonsArray[1].getText() == "X"
-            ) {
-            
-        }
+        //X wins conditions
 
+        if(
+            (buttonsArray[0].getText() == "X") &&
+            (buttonsArray[3].getText() == "X") &&
+            (buttonsArray[6].getText() == "X") 
+            ) {
+            Xwins(0, 3, 6); // vertical first row
+        }
+        
+        if(
+            (buttonsArray[1].getText() == "X") &&
+            (buttonsArray[4].getText() == "X") &&
+            (buttonsArray[7].getText() == "X") 
+            ) {
+            Xwins(1, 4, 7); // vertical second row
+        }
+        
+        if(
+            (buttonsArray[2].getText() == "X") &&
+            (buttonsArray[5].getText() == "X") &&
+            (buttonsArray[8].getText() == "X") 
+            ) {
+            Xwins(2, 5, 8); // vertical third row
+        }
+        
+        if(
+            (buttonsArray[0].getText() == "X") &&
+            (buttonsArray[1].getText() == "X") &&
+            (buttonsArray[2].getText() == "X") 
+            ) {
+            Xwins(0, 1, 2); // horizontal first row
+        }
+        
+        if(
+            (buttonsArray[3].getText() == "X") &&
+            (buttonsArray[4].getText() == "X") &&
+            (buttonsArray[5].getText() == "X") 
+            ) {
+            Xwins(3, 4, 5); // horizontal second row
+        }
+        
+        if(
+            (buttonsArray[6].getText() == "X") &&
+            (buttonsArray[7].getText() == "X") &&
+            (buttonsArray[8].getText() == "X") 
+            ) {
+            Xwins(6, 7, 8); // horizontal third row
+        }
+        
+        if(
+            (buttonsArray[0].getText() == "X") &&
+            (buttonsArray[4].getText() == "X") &&
+            (buttonsArray[8].getText() == "X") 
+            ) {
+            Xwins(0, 4, 8); // diagonal left
+        }
+        
+        if(
+            (buttonsArray[2].getText() == "X") &&
+            (buttonsArray[4].getText() == "X") &&
+            (buttonsArray[6].getText() == "X") 
+            ) {
+            Xwins(2, 4, 6); // diagonal right
+        }
+        
+
+
+        // O wins condition
+
+        
+        if(
+            (buttonsArray[0].getText() == "O") &&
+            (buttonsArray[3].getText() == "O") &&
+            (buttonsArray[6].getText() == "O") 
+            ) {
+            Owins(0, 3, 6); // vertical first row
+        }
+        
+        if(
+            (buttonsArray[1].getText() == "O") &&
+            (buttonsArray[4].getText() == "O") &&
+            (buttonsArray[7].getText() == "O") 
+            ) {
+            Owins(1, 4, 7); // vertical second row
+        }
+        
+        if(
+            (buttonsArray[2].getText() == "O") &&
+            (buttonsArray[5].getText() == "O") &&
+            (buttonsArray[8].getText() == "O") 
+            ) {
+            Owins(2, 5, 8); // vertical third row
+        }
+        
+        if(
+            (buttonsArray[0].getText() == "O") &&
+            (buttonsArray[1].getText() == "O") &&
+            (buttonsArray[2].getText() == "O") 
+            ) {
+            Owins(0, 1, 2); // horizontal first row
+        }
+        
+        if(
+            (buttonsArray[3].getText() == "O") &&
+            (buttonsArray[4].getText() == "O") &&
+            (buttonsArray[5].getText() == "O") 
+            ) {
+            Owins(3, 4, 5); // horizontal second row
+        }
+        
+        if(
+            (buttonsArray[6].getText() == "O") &&
+            (buttonsArray[7].getText() == "O") &&
+            (buttonsArray[8].getText() == "O") 
+            ) {
+            Owins(6, 7, 8); // horizontal third row
+        }
+        
+        if(
+            (buttonsArray[0].getText() == "O") &&
+            (buttonsArray[4].getText() == "O") &&
+            (buttonsArray[8].getText() == "O") 
+            ) {
+            Owins(0, 4, 8); // diagonal left
+        }
+        
+        if(
+            (buttonsArray[2].getText() == "O") &&
+            (buttonsArray[4].getText() == "O") &&
+            (buttonsArray[6].getText() == "O") 
+            ) {
+            Owins(2, 4, 6); // diagonal right
+        }
+        
+
+      
     }
 
     public void Xwins(int a,int b,int c){
@@ -123,13 +295,14 @@ public class Main extends JFrame implements ActionListener{
         title.setText("Player O Wins");
     }
 
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        
-        
-
+    public void draw(){
+        title.setText("Draw");
+        for (int i = 0; i < 9; i++) {
+            buttonsArray[i].setEnabled(false);
+        }
     }
+
+    
 
 
     public static void main(String[] args) {
